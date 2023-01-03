89207cca4acc6567ee7e6d82000f7aee02114a4d
Temphoyos <87236758+temphoyos@users.noreply.github.com>
2023-01-03 14:07:21 +0100

    Add files via upload
    Original tracker.js file. Adds an eventListener to the main body and listens to certain clic events. An event handler executes dataLayer pushes when conditions are met.

---
+++ tracker.js
@@ -0,0 +1,40 @@
+try{ 
+	
+    
+    var trackedBody = window.document.body;
+    trackedBody.addEventListener('click', eventHandler, true);
+    function eventHandler(e){
+
+    if(e.target['type'] === 'button'){
+
+        const buttonArray = Array.from(document.querySelectorAll("[type='button']"));
+
+        for(var i = 0; i < buttonArray.length; i++){
+
+            if(e.target === buttonArray[i]){
+
+                j = i+1;
+
+                dataLayer.push({
+
+                    code_block_position:j,
+                    event:'ev/copy_button_clicked'
+        
+                });
+
+                break;
+            }
+        }
+    }
+    else if(e.target.tagName === 'A' && e.target.closest('ul').id === 'menu-menu-1'){
+
+        dataLayer.push({
+
+            item_clicked:e.target.innerText.toLowerCase(),
+            event:'ev/top_menu'
+        });
+    }
+    }
+    console.log("I'm here!");
+
+}catch(e){}
